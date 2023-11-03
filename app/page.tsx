212b2ae5193f3c0cc6447544a6f5e8a506a0c37a
Eliran <eliranschool@gmail.com>
2023-11-03 19:20:37 +0200

    m

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,60 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { useRouter } from 'next/navigation'
-import WhatsappIcon from '../public/whatsapp-social-media-svgrepo-com.svg'
-import { useState } from 'react'
-import Link from 'next/link'
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
+import WhatsappIcon from "../public/whatsapp-social-media-svgrepo-com.svg";
+import { ClipboardPasteIcon } from "lucide-react";
+import { useState } from "react";
+import Link from "next/link";
 export default function Home() {
-  const [phoneNumber, setPhoneNumber] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-  const router = useRouter()
-
+  const router = useRouter();
+  const formatNumber = (number: String) => {
+    const whitespaceRemoved = number.replace(/\-/g, "");
+    return whitespaceRemoved;
+  };
   const isValidNumber = (number: String) => {
-    if(number.length == 10 || number.length == 9)
-    {
-      console.log("good")
-      return true
-
+    const formattedNumber = formatNumber(number);
+    if (formattedNumber.length == 10 || formattedNumber.length == 9) {
+      console.log("good");
+      return true;
     }
-  return false
+
+    return false;
+  };
+
+  const pastePhoneNumber = async () => {
+    const number = await navigator.clipboard.readText()
+    setPhoneNumber(number)
   }
+
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='flex'>
+      <div className="flex">
+      <Button className="bg-white" onClick={() => pastePhoneNumber()}>
+        <ClipboardPasteIcon color="black"/>
+          </Button>
         <Input
-        value={phoneNumber}
-        onChange={(e) => setPhoneNumber(e.target.value)}
-        placeholder='05X-XXXXXXX'
-        >
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          placeholder="05X-XXXXXXX"
+        ></Input>
 
-        </Input>
-        
         <Link
-                href={isValidNumber(phoneNumber) ? `https://wa.me/+972${phoneNumber}`: {}}
-                className='ml-3'
+          href={
+            isValidNumber(phoneNumber) ? `https://wa.me/+972${formatNumber(phoneNumber)}` : {}
+          }
+          className="ml-3"
         >
-          
-        <Button
-        className="bg-white"
-        >
-          <Image
-          src={WhatsappIcon}
-          alt="sd" 
-          height={30}
-          width={30}
-          />
-
-        </Button>
+          <Button className="bg-white">
+            <Image src={WhatsappIcon} alt="sd" height={60} width={60} />
+          </Button>
         </Link>
-
       </div>
     </main>
-  )
+  );
 }
